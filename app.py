df1b1c3e4d7f3164c992ef161ff4286f82f92c4e
Student User <student@workstation.lab.example.com>
2022-11-05 12:02:29 -0400

    chng

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is the awesome app EVER, speaking with brad and sahdev."
+    return "This is the myapp"
  
 
 @app.route("/healthz")
@@ -19,4 +19,4 @@ def healthz():
     return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port='8000')
